64de44c7392b2a8031b69d8372a5897a4cb7571e
Camilla Krag Jensen <camilla@reload.dk>
2015-02-18 19:46:59 +0100

    Publish script now deletes unused files on the remote.

--- publish.sh
+++ publish.sh
@@ -6,6 +6,10 @@ sass=$(gulp sass | grep gulp-sass)
 if [ -n "$sass" ]; then echo "ERROR: SASS could not be compiled"; exit 1; fi
 popd
 
+# Not sure if Sculpin does this by itself before a generate to prod, but better
+# safe than sorry.
+rm -r output_prod
+
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
@@ -16,7 +20,7 @@ $(git log -1 --pretty=format:'%s (%ci)' --abbrev-commit)
 https://github.com/naxoc/sculpin-blog/commit/$(git rev-parse HEAD)" > output_prod/VERSION.txt
 
 # "almond" is set up in my .ssh/config.
-rsync -avz output_prod/ almond:/var/www/html/naxoc.net
+rsync -avz --delete output_prod/ almond:/var/www/html/naxoc.net
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
 
 # Remove the local "deployment" file.
