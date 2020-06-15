542ad902f06fdedfa20c36a6b472e95454dd43c6
Camilla Krag Jensen <camillakrag@gmail.com>
2020-06-15 20:53:23 +0200

    Update Gulp and Bourbon

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is my blog. The built output can be seen on [naxoc.net](http://naxoc.net). It uses Gulp for the theme because I wanted to play around with that (the folder called pure is the 'theme').
 
 ### To install:
-[Install Sculpin](https://sculpin.io/getstarted/) - then run:
+Make sure you have Composer and npm installed and then run:
 
 ```
 composer install
@@ -20,7 +20,7 @@ vendor/bin/sculpin generate --watch --server
 
 In the pure directory:
 ```
-npm run develop
+npm run watch
 ```
 
 The blog will be available locally on your machine at [http://localhost:3000](http://localhost:3000) - or check the output from Gulp to se urls.
--- pure/gulpfile.js
+++ pure/gulpfile.js
@@ -1,34 +1,40 @@
-'use strict';
+const gulp = require("gulp"),
+  bourbon = require("bourbon").includePaths,
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps");
 
-var gulp = require("gulp"),
-bourbon = require("bourbon").includePaths,
-sass = require("gulp-sass"),
-sourcemaps = require('gulp-sourcemaps');
-
-var paths = {
-  sassFiles: 'scss/*.scss',
-  cssDir: '../source/css',
-  mapsDir: '../maps',
-  outputCss: '../output_dev/css/*.css'
+const paths = {
+  sassFiles: "scss/*.scss",
+  cssDir: "../source/css",
+  mapsDir: "../maps",
+  outputCss: "../output_dev/css/*.css",
 };
 
-gulp.task('sass', function () {
-  return gulp.src(paths.sassFiles)
-    .pipe(sourcemaps.init())
+gulp.task(
+  "sass",
+  gulp.series(() =>
+    gulp
+      .src(paths.sassFiles)
+      .pipe(sourcemaps.init())
 
-    .pipe(sass({
-      includePaths: bourbon,
-      require: ['normalize-scss'],
-      outputStyle: 'compressed',
-      errLogToConsole: true
-    }))
+      .pipe(
+        sass({
+          includePaths: bourbon,
+          require: ["normalize-scss"],
+          outputStyle: "compressed",
+          errLogToConsole: true,
+        })
+      )
 
-    .pipe(sourcemaps.write(paths.mapsDir))
-    .pipe(gulp.dest(paths.cssDir));
-});
+      .pipe(sourcemaps.write(paths.mapsDir))
+      .pipe(gulp.dest(paths.cssDir))
+  )
+);
 
-
-// Default task to be run with `gulp`
-gulp.task('default', ['sass'], function () {
-  gulp.watch(paths.sassFiles, ['sass']);
+gulp.task('watch', function () {
+  gulp.watch(
+    [paths.sassFiles],
+    { usePolling: true, interval: 1000 },
+    gulp.series('sass')
+  );
 });
